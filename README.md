8ea86ce7f58c0b93eeecaeef2092c2bdafb3a1f1
Ayoub-Tehari <ayoubtehari01@gmail.com>
2024-05-31 09:19:12 +0200

    registration form

---
+++ README.md
@@ -0,0 +1,3 @@
+composer require serializer
+composer require symfonycasts/verify-email-bundle
+composer require symfony/security-bundle
---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/api/registration', name: 'app_registration', methods: ['POST'])]
+    public function index(Request $request, EntityManagerInterface $entityManager, serializerInterface $serializer, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user_data = $request->getContent();
+        $user = $serializer->deserialize($user_data, User::class, 'json');
+        // ... e.g. get the user data from a registration form
+        $plaintextPassword = $user->getPassword();
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return $this->json([
+            'user' => $user,
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/RegistrationController.php'
+        ]);
+    }
+}
